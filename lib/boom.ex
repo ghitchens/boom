1b8bae47f40c63e49e533f616773ddcfba386789
Garth Hitchens <ghitchens@me.com>
2017-03-01 01:00:10 -0500

    blinking

---
+++ lib/boom.ex
@@ -0,0 +1,29 @@
+defmodule Boom do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # worker(Boom.Worker, [arg1, arg2, arg3]),
+      worker(Task, [fn -> blinker() end])
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Boom.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  def blinker() do
+    Nerves.Leds.set green: true
+    :timer.sleep 200
+    Nerves.Leds.set green: false
+    :timer.sleep 200
+    blinker()
+  end
+
+end

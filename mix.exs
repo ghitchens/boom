61cc090bb0b4565fadce8630ceba1af2ad9ca33a
Garth Hitchens <ghitchens@me.com>
2017-03-01 02:44:15 -0500

    firmware updates

--- mix.exs
+++ mix.exs
@@ -30,6 +30,7 @@ defmodule Boom.Mixfile do
     [{:nerves, "~> 0.4.0"},
      {:nerves_interim_wifi, "~> 0.1"},
      {:logger_multicast_backend, "~> 0.2"},
+     {:nerves_firmware_http, "~>0.3.1"},
      {:nerves_leds, "~> 0.8.0"}]
   end
 
